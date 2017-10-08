b097d5cc67589baefba35c7c1ca63bd4a08c4e31
Tom Butler <tom@r.je>
2017-10-08 16:54:32 +0100

    initial commit

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Web Page!</title>
+    <link rel="stylesheet" href="ex2.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Heading</h1>
+    </header>
+
+    <div class="table">
+    <nav>
+      <ul>
+        <li>
+          <a href="index.php">Home</a>
+        </li>
+        <li>
+          <a href="about.php">About</a>
+        </li>
+        <li>
+          <a href="contact.php">Contact</a>
+        </li>
+      </ul>
+
+    </nav>
+    <main>
+      <p>I'm a student at the University of Northampton</p>
+    </main>
+
+    <div>
+        Right hand side
+    </div>
+    </div>
+
+    <footer>
+      &copy; Your Name 2017
+    </footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Web Page!</title>
+    <link rel="stylesheet" href="ex2.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Heading</h1>
+    </header>
+
+    <div class="table">
+    <nav>
+      <ul>
+        <li>
+          <a href="index.php">Home</a>
+        </li>
+        <li>
+          <a href="about.php">About</a>
+        </li>
+        <li>
+          <a href="contact.php">Contact</a>
+        </li>
+      </ul>
+
+    </nav>
+    <main>
+      <p>Welcome to my website</p>
+    </main>
+
+    <div>
+        Right hand side
+    </div>
+    </div>
+
+    <footer>
+      &copy; Your Name 2017
+    </footer>
+  </body>
+</html>
